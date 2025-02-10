fc4cfeaab9be485748aed855e10687632e9942bd
Ayman ALKHATIB <aemn22384@gmail.com>
2025-02-10 11:02:12 +0100

    initial spring boot app

---
+++ src/test/java/com/greata/twitly/TwitlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.greata.twitly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitlyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
