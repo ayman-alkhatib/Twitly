ed31f6706d785c2e5382d2fe26f57df9f8e95af8
Ayman ALKHATIB <aemn22384@gmail.com>
2025-02-10 13:49:02 +0100

    add the schema for the database and the DatabaseConfig

---
+++ src/main/resources/Schema.sql
@@ -0,0 +1,51 @@
+CREATE DATABASE  IF NOT EXISTS twitly_db;
+
+USE twitly_db;
+
+CREATE TABLE users (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    created_it TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE tweets (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    content TEXT NOT NULL,
+    created_it TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE comments (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id BIGINT NOT NULL,
+    tweet_id BIGINT NOT NULL,
+    content TEXT NOT NULL,
+    created_it TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (tweet_id) REFERENCES tweets(id) ON DELETE CASCADE,
+);
+
+CREATE TABLE likes (
+     user_id BIGINT NOT NULL,
+     tweet_id BIGINT NOT NULL,
+     created_it TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+     PRIMARY KEY (user_id,tweet_id),
+     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+     FOREIGN KEY (tweet_id) REFERENCES tweets(id) ON DELETE CASCADE,
+);
+
+CREATE TABLE followers (
+      follower_id BIGINT NOT NULL,
+      following_id BIGINT NOT NULL,
+      created_it TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      PRIMARY KEY (follower_id,following_id),
+      FOREIGN KEY (follower_id) REFERENCES users(id) ON DELETE CASCADE,
+      FOREIGN KEY (following_id) REFERENCES users(id) ON DELETE CASCADE,
+);
+
+
+
+
+
